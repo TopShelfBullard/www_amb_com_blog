7bf8132b8639b1c0a347f839235a787782d347f0
TopShelfBullard <bullardmitchell@yahoo.com>
2013-10-05 21:34:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+AMB - Blog
+============================
+
+This rails project is to become a personal blog to link to my homepage. It will initially be based 
+on the blog tutorial in the Rails Guides.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '4c3d1cb598fe1b44bb6f7ca8e7f761de3d0dcacaaafc160cf65c3572c822f8c68765992e1541e88da6ad515d33144e104bfb1e7d86b3d98a6554a14286839ff7'
